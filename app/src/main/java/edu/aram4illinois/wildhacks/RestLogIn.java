9c6b82e807c80fa90f41a61073e70aa06b31cc39
Abdul <abdulbagasra@gmail.com>
2016-11-19 16:01:34 -0600

    Fix EditText

--- app/src/main/java/edu/aram4illinois/wildhacks/RestLogIn.java
+++ app/src/main/java/edu/aram4illinois/wildhacks/RestLogIn.java
@@ -2,13 +2,34 @@ package edu.aram4illinois.wildhacks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class RestLogIn extends AppCompatActivity {
+    EditText emailEditText;
+    EditText passEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rest_log_in);
+
+        emailEditText = (EditText) findViewById(R.id.emailEditText);
+        passEditText = (EditText) findViewById(R.id.passwordEditText);
+
+        emailEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                emailEditText.getText().clear();
+            }
+        });
+
+        passEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                passEditText.getText().clear();
+            }
+        });
 
     }
 }
