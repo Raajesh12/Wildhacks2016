95b3bf98ee3a9bcbd7083e1c0a2401828e16dbc3
Raajesh Arunachalam <Raajesh Arunachalam>
2016-11-19 16:29:05 -0600

    latest version

--- app/src/main/java/edu/aram4illinois/wildhacks/RestLogIn.java
+++ app/src/main/java/edu/aram4illinois/wildhacks/RestLogIn.java
@@ -1,13 +1,17 @@
 package edu.aram4illinois.wildhacks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RestLogIn extends AppCompatActivity {
     EditText emailEditText;
     EditText passEditText;
+    Button proceedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,7 @@ public class RestLogIn extends AppCompatActivity {
 
         emailEditText = (EditText) findViewById(R.id.emailEditText);
         passEditText = (EditText) findViewById(R.id.passwordEditText);
+        proceedButton = (Button) findViewById(R.id.button);
 
         emailEditText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +36,25 @@ public class RestLogIn extends AppCompatActivity {
             }
         });
 
+        proceedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailAddress = emailEditText.getText().toString();
+                String password = passEditText.getText().toString();
+                if(emailAddress.equals("john@papajohns.com") && password.equals("papa")){
+                    Intent intent = new Intent(v.getContext(), RestList.class);
+                    startActivity(intent);
+                    emailEditText.getText().clear();
+                    passEditText.getText().clear();
+                }
+                else{
+                    Toast.makeText(v.getContext(), "Invalid Username or Password!",
+                            Toast.LENGTH_LONG).show();
+                    emailEditText.getText().clear();
+                    passEditText.getText().clear();
+                }
+            }
+        });
     }
+
 }
